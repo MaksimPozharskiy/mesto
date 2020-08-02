50d0c79bc9bfa982bb388bce27a826400115c92a
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-08-02 14:13:36 +0300

    feat: добавить редактирование профиля через попап

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+let popup = document.querySelector('.popup')
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = popup.querySelector('.popup__button-close');
+let popupForm = popup.querySelector('.popup__form');
+
+// Закрытие/открытие попапа по кнопкам
+let popupToggle = () => {
+  popup.classList.toggle('popup_opened')
+  fillValueForm();
+}
+
+// Закрытие попапа по клике на оверлей
+let closePopup = (event) => { 
+  if (event.target !== event.currentTarget) return;   
+  popupToggle(event);
+}
+
+// Обработчик формы 
+let formSubmitHandler = event => {
+  event.preventDefault();
+
+  let nameInput = popup.querySelector('.popup__name');
+  let professionInput = popup.querySelector('.popup__profession');
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = professionInput.value;
+
+  popupToggle();
+}
+
+// Функция для изначального заполнения значений полей формы
+let fillValueForm = () => {
+  let nameInput = popup.querySelector('.popup__name');
+  let professionInput = popup.querySelector('.popup__profession');
+  nameInput.value = profileName.textContent;
+  professionInput.value =  profileProfession.textContent;
+}
+
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+popup.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', formSubmitHandler); // Кнопка "Сохранить"

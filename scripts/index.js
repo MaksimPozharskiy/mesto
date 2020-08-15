c385c77721da20f0b8203cca9e3153a3f8a35740
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-08-15 13:31:50 +0300

    refactor: добавить генерацию изначальных изображений из массива объектов

--- scripts/index.js
+++ scripts/index.js
@@ -42,3 +42,49 @@ popupCloseButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', closePopup);
 
 popupForm.addEventListener('submit', formSubmitHandler); // Кнопка "Сохранить"
+
+// Генерация первых 6 карточек из коробки
+
+const initialCards =[
+  {
+    name: 'Конь на лугу',
+    link: './images/kate-nesmieian-rRXUtG1Kbqs-unsplash.jpg'
+  },
+  {
+    name: 'Лошадки',
+    link: './images/mark-neal-Am8FAT_PoJM-unsplash.jpg'
+  },
+  {
+    name: 'Милая лошадь',
+    link: './images/navid-bazari-Vw4oefoH4Iw-unsplash.jpg'
+  },
+  {
+    name: 'Разговор коней',
+    link: './images/raphael-wicker-P6JRr7-FxLw-unsplash.jpg'
+  },
+  {
+    name: 'Утренний завтрак',
+    link: './images/rich-dahlgren--MMRAIrqgUE-unsplash.jpg'
+  },
+  {
+    name: 'Внимательный конь',
+    link: './images/santiago-martin-7NC_LcUaky8-unsplash.jpg'
+  }
+]
+
+const gridCardTemplate = document.querySelector('#grid-item').content;
+const gridPhotos = document.querySelector('.grid-photos');
+
+const renderInitialCards = (initialCards) => {
+  initialCards.forEach(card => {
+    const gridCardElement = gridCardTemplate.cloneNode(true);
+    gridCardElement.querySelector('.grid-item__image').src = card.link;
+    gridCardElement.querySelector('.grid-item__name').textContent = card.name;
+
+    gridPhotos.append(gridCardElement);
+  });
+}
+renderInitialCards(initialCards);
+
+
+

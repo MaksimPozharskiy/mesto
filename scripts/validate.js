9e7894ccdbba5617758ba44a4202091123b20a73
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-09-01 21:53:36 +0300

    feat: добавлена структуризация в настройках валидации форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,80 +1,81 @@
-const formEditProfileElement = document.querySelector('form[name="addCard"]');
-
-const formAddCardElement = document.querySelector('form[name="editProfile"]');
-
-// Показать ошибку
-const showInputError = (formSelector, inputSelector, errorMessage) => {
-  const errorElement = formSelector.querySelector(`#${inputSelector.id}-error`);
-  inputSelector.classList.add('popup__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
-}
-// скрыть ошибку
-const hideInputError = (formSelector, inputSelector) => {
-  const errorElement = formSelector.querySelector(`#${inputSelector.id}-error`);
-  inputSelector.classList.remove('popup__input_type_error');
-  errorElement.classList.remove('popup__input-error_active');
-  errorElement.textContent = '';
-}
-// Проверить валидность формы
-const checkInputValidity = (formSelector, inputSelector) => {
-  if (!inputSelector.validity.valid) {
-    showInputError(formSelector, inputSelector, inputSelector.validationMessage);
-  } else {
-    hideInputError(formSelector, inputSelector);
-  }
-}
-// Проверка валидности поля
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputSelector) => {
-    return !inputSelector.validity.valid;
-  });
-}
-// переключение disabled
-const toggleButtonState = (inputList, submitButtonSelector) => {
-  if (hasInvalidInput(inputList)) {
-    submitButtonSelector.classList.add('popup__button-save_inactive');
-    submitButtonSelector.setAttribute('disabled', '');
-  } else {
-    submitButtonSelector.classList.remove('popup__button-save_inactive');
-    submitButtonSelector.removeAttribute('disabled', '');
-  }
-}
-
-const setEventListeners = (formSelector) => {
-  const inputList = Array.from(formSelector.querySelectorAll('.popup__input'));
-  const submitButtonSelector = formSelector.querySelector('.popup__button-save');
-
-  toggleButtonState(inputList, submitButtonSelector);
-
-  inputList.forEach((inputSelector) => {
-    inputSelector.addEventListener('input', function () {
-
-      checkInputValidity(formSelector, inputSelector);
-      toggleButtonState(inputList, submitButtonSelector);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-  formList.forEach((formSelector) => {
-    formSelector.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-    
-    formList.forEach((form) => {
-    setEventListeners(form);
-    });
-  });
-};
-
-// При вызове функция принимает объект настрое форм на странице
-enableValidation({
+const settingsForm = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button-save',
   inactiveButtonClass: 'popup__button-save_inactive',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__input-error_active'
-});
+}
+// Показать ошибку
+const showInputError = (formSelector, inputSelector, errorMessage, {errorClass, inputErrorClass}) => {
+  const errorElement = formSelector.querySelector(`#${inputSelector.id}-error`);
+  inputSelector.classList.add(inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(errorClass);
+}
+
+// скрыть ошибку
+const hideInputError = (formSelector, inputSelector, {errorClass, inputErrorClass}) => {
+  const errorElement = formSelector.querySelector(`#${inputSelector.id}-error`);
+  inputSelector.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = '';
+}
+
+// Проверить валидность формы
+const checkInputValidity = (formSelector, inputSelector) => {
+  if (!inputSelector.validity.valid) {
+    showInputError(formSelector, inputSelector, inputSelector.validationMessage, settingsForm);
+  } else {
+    hideInputError(formSelector, inputSelector, settingsForm);
+  }
+}
+
+// Проверка валидности поля
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputSelector) => {
+    return !inputSelector.validity.valid;
+  });
+}
+
+// переключение disabled
+const toggleButtonState = (inputList, submitButtonSelector, {inactiveButtonClass}) => {
+  if (hasInvalidInput(inputList)) {
+    submitButtonSelector.classList.add(inactiveButtonClass);
+    submitButtonSelector.setAttribute('disabled', '');
+  } else {
+    submitButtonSelector.classList.remove(inactiveButtonClass);
+    submitButtonSelector.removeAttribute('disabled', '');
+  }
+}
+
+const setEventListeners = (form, {inputSelector, submitButtonSelector}) => {
+  const inputList = Array.from(form.querySelectorAll(inputSelector));
+  const submitButton = form.querySelector(submitButtonSelector);
+
+  toggleButtonState(inputList, submitButton, settingsForm);
+
+  inputList.forEach((inputSelector) => {
+    inputSelector.addEventListener('input', function () {
+
+      checkInputValidity(form, inputSelector);
+      toggleButtonState(inputList, submitButton, settingsForm);
+    });
+  });
+}
+
+const enableValidation = ({formSelector}) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  formList.forEach((item) => {
+    item.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    
+    formList.forEach((item) => {
+      setEventListeners(item, settingsForm);
+    });
+  });
+}
+
+// При вызове функция принимает объект настрое форм на странице
+enableValidation(settingsForm);
